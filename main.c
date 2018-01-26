6fe476cb56700c2c89c6cb6113be941b2e7367cd
衍 <34103504+Jixiong@users.noreply.github.com>
2018-01-26 18:39:29 +0100

    Add files via upload
    La dernier changement pour la séance de robot

--- main.c
+++ main.c
@@ -20,18 +20,21 @@
 void init_suivi_ligne(void);
 int suivi1(void);
 int suivi2(void);
-
+void init_IR(void);
 /*
  * main.c
  */
--èint main(void) {
+ int main(void) {
 	volatile int i,j;
 	volatile int suivi_gauche, suivi_droit;
 
-	volatile int resultat;
+	volatile int resultat=0;
 	volatile double tension;
 
 	volatile int vitess;
+	volatile int vitess_droite;
+
+
 	volatile int butoir = 0;//quand butoir = 0 accélération
 
 
@@ -41,11 +44,14 @@ int suivi2(void);
 
     vitess = 0; //Ici, c'est pour initialiser la valeur de vitess à 60
     ADC_init();
+    init_IR();
 
     init_moteurs();
     puissance_moteurs(vitess,vitess);
     moteurs_avancer();
-    puissance_moteurs(vitess,vitess);
+    //puissance_moteurs(vitess,vitess);
+
+    for (i=0;i<30000;i++);
 
     while(1){
     	ADC_Demarrer_conversion(0);
@@ -53,36 +59,67 @@ int suivi2(void);
 
 
     	//la période de accélération
-    	if((vitess<95)&&(butoir==0)){
+    	if((vitess < 80)&&(butoir==0)){
     			vitess += 5;
-    			puissance_moteurs(vitess,vitess);
+    			if(vitess > 50){
+    				vitess_droite = vitess;
+    			}
+    			else if(vitess > 70){
+    				vitess_droite = vitess-1;
+    			}
+    			else{
+    			vitess_droite = vitess;
+    			}
+    			puissance_moteurs(vitess,vitess_droite);
+
+
     	}
 
     	//la période de ralentir
     	if ((vitess >= 20)&&(butoir == 1)){
-    			vitess -= 1;
+    			vitess -= 10;
     		    puissance_moteurs(vitess,vitess);
     	}
 
 
 
 
-    	//Pour changer la
-        if (resultat > 150){
+    	//Pour changer la mode d'acce
+        if (resultat > 400){
         	butoir = 1;
     	}
 
+        if (resultat < 400){
+            //butoir = 0 ;
+        }
+
+
+
+
+
         //Pour arrêtre le robot
-    	if (resultat > 250){
-    		vitess = 0;
+    	if (resultat > 320){
+
     		puissance_moteurs(0, 0);
     	}
 
 
-        for (i=0;i<5000;i++);
+        for (i=0;i<3000;i++);
     	}
 
 	return 0;
+}
+
+
+
+
+void init_IR(void){
+
+	P1SEL &= ~BIT0;
+	P1SEL2 &= ~BIT0;
+	P1DIR &=~ BIT0;
+
+
 }
 
 int suivi1(void) {
